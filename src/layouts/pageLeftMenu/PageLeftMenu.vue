fb40a3ca671512252355210e1d39a9e40a12a0f9
sun <1844528595@qq.com>
2021-03-25 09:44:56 +0800

    test2

--- src/layouts/pageLeftMenu/PageLeftMenu.vue
+++ src/layouts/pageLeftMenu/PageLeftMenu.vue
@@ -2,87 +2,15 @@
   <el-aside :width="leftWidth + 'px'" class="left-menu">
     <div class="menu-box">
     <el-menu :default-openeds="['1']" :collapse="isShow">
-      <el-submenu index="1">
+      <template v-for="item in menuArray">
+        <el-submenu :index="item.id" :key="item.id">
         <template slot="title">
-          <i class="el-icon-message"></i>
-          <span slot="title">首页</span>
+          <i :class="item.icon"></i>
+          <router-link slot="title" :to="item.path">{{item.title}}</router-link>
+          <!-- <span ></span> -->
           </template>
       </el-submenu>
-      <el-submenu index="2">
-        <template slot="title">
-          <i class="el-icon-menu"></i>
-          <span slot="title">运营管理</span>
-          </template>
-      </el-submenu>
-      <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-menu"></i>
-          <span slot="title">用户管理</span>
-          </template>
-      </el-submenu>
-
-       <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-menu"></i>
-          <span slot="title">用户管理</span>
-          </template>
-      </el-submenu>
-
-       <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-menu"></i>
-          <span slot="title">用户管理</span>
-          </template>
-      </el-submenu>
-
-       <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-menu"></i>
-          <span slot="title">用户管理</span>
-          </template>
-      </el-submenu>
-
-       <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-menu"></i>
-          <span slot="title">用户管理</span>
-          </template>
-      </el-submenu>
-
-       <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-menu"></i>
-          <span slot="title">用户管理</span>
-          </template>
-      </el-submenu>
-
-       <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-menu"></i>
-          <span slot="title">用户管理</span>
-          </template>
-      </el-submenu>
-
-       <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-menu"></i>
-          <span slot="title">用户管理</span>
-          </template>
-      </el-submenu>
-
-       <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-menu"></i>
-          <span slot="title">用户管理</span>
-          </template>
-      </el-submenu>
-
-       <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-menu"></i>
-          <span slot="title">用户管理</span>
-          </template>
-      </el-submenu>
+      </template>
 
       <el-submenu index="3">
         <template slot="title">
@@ -102,7 +30,7 @@
           <el-menu-item index="3-4-1">选项4-1</el-menu-item>
         </el-submenu>
       </el-submenu>
-    </el-menu>
+    </el-menu>  
     </div>
   </el-aside>
 </template>
@@ -123,10 +51,99 @@ data(){
       {
           icon:"el-icon-edit-outline",
           title:"运营管理",
-          id:1,
+          id:2,
           path:"/operate",
           child:[]
+      },
+      {
+          icon:"el-icon-date",
+          title:"用户管理",
+          id:3,
+          path:"/admin",
+          child:[]
+      },
+      {
+          icon:"el-icon-tickets",
+          title:"雨伞免费",
+          id:4,
+          path:"/umbrella",
+          child:[]
+      },{
+          icon:"el-icon-discover",
+          title:"地图分布",
+          id:5,
+          path:"/map",
+          child:[]
+      },{
+          icon:"el-icon-eleme",
+          title:"伞架管理",
+          id:6,
+          path:"/umbrellaStand",
+          child:[]
+      },
+      {
+          icon:"el-icon-eleme",
+          title:"商家管理",
+          id:7,
+          path:"/business",
+          child:[]
+      },
+      {
+          icon:"el-icon-eleme",
+          title:"渠道管理",
+          id:8,
+          path:"/channel",
+          child:[]
+      },
+      {
+          icon:"el-icon-eleme",
+          title:"优惠卷管理",
+          id:9,
+          path:"/coupon",
+          child:[]
+      },
+      {
+          icon:"el-icon-eleme",
+          title:"消息管理",
+          id:10,
+          path:"/message",
+          child:[]
+      },
+      {
+          icon:"el-icon-eleme",
+          title:"数据统计",
+          id:11,
+          path:"/dataCount",
+          child:[]
+      },
+      {
+          icon:"el-icon-eleme",
+          title:"乐天订单",
+          id:12,
+          path:"/order",
+          child:[]
+      },
+      {
+          icon:"el-icon-eleme",
+          title:"系统管理",
+          id:11,
+          path:"/system",
+          child:[
+              {
+          icon:"el-icon-eleme",
+          title:"数据统计",
+          id:11,
+          path:"/dataCount",
+          child:[]
+      },
+      {
+          icon:"el-icon-eleme",
+          title:"乐天订单",
+          id:12,
+          path:"/order",
+          child:[]
       }]
+      },]
     }
   },
   methods:{
